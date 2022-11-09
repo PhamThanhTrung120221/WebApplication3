4ec7a98e98d8a89542df9d4acc6cf326398efaf4
PhamThanhTrung120221 <phamthanhtrung1721@gmail.com>
2022-11-09 21:46:29 +0700

    update

---
+++ Controllers/THUOCController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    /// <summary>
+    /// controller quan ly phieu nhap
+    /// </summary>
+
+    public class PTHUOCController : Controller
+    {
+        public ttcn_btlEntities ttcn_BtlEnt { get; set; }
+        // GET: PNhap
+        public ActionResult Index()
+        {
+            ttcn_BtlEnt = new ttcn_btlEntities();
+            ViewBag.Title = "Quan ly phieu nhap";
+            return View(ttcn_BtlEnt);
+        }
+    }
+}
